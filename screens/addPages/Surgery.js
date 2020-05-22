6c03e3e0fb7ec1ff3191e616469cb41bbb0201f2
AyaaG <ayaomrana@gmail.com>
2020-05-22 20:44:59 +0000

    Erroor

--- screens/addPages/Surgery.js
+++ screens/addPages/Surgery.js
@@ -1,12 +1,74 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
-
+import { Text, StyleSheet, View, TouchableOpacity } from 'react-native'
+import logo from '../addPageIcons/surgery.png';
+import { appStyle } from '../styles/globalStyle';
+import { Input } from 'react-native-elements'
+import * as Animatable from 'react-native-animatable';
 export default class Surgery extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nomchirurgie: '',
+            faitle: '',
+            endroit: '',
+            pardocteur: ''
+        } }
+
     render() {
         return (
             <View style={styles.container} >
-                <Text> Chirurgie Pages </Text>
-            </View>
+               
+       
+             <View style={{ marginBottom: 50 }} >
+             <Animatable.Image
+                 source={logo}
+                 animation="pulse"
+                 easing="ease-out"
+                 iterationCount="infinite" />
+         </View>
+         <Text> Nom de chirurgie: </Text>
+             <Input
+             style={appStyle.input}
+             label=''
+             placeholder=''
+             autoCapitalize='none'
+             onChangeText={(nomchirurgie) => this.setState({nomchirurgie})}
+             value={this.state.nomchirurgie}
+         />
+         <View style = {styles.InputCont}>
+          <Text> Fait le : </Text>
+             <Input
+             style={appStyle.input}
+             label=''
+             placeholder=''
+             autoCapitalize='none'
+             onChangeText={(faitle) => this.setState({faitle})}
+             value={this.state.faitle}
+         />
+          <Text> a : </Text>
+             <Input
+             style={appStyle.input}
+             label=''
+             placeholder=''
+             autoCapitalize='none'
+             onChangeText={(endroit) => this.setState({endroit})}
+             value={this.state.endroit}
+         />
+         </View>
+         <Text> Par Docteur : </Text>
+             <Input
+             style={appStyle.input}
+             label=''
+             placeholder=''
+             autoCapitalize='none'
+             onChangeText={(pardocteur) => this.setState({pardocteur})}
+             value={this.state.pardocteur}
+         />
+         <TouchableOpacity style={appStyle.button} onPress={this.loginUser}>
+                    <Text style={appStyle.btnText} > Enregistrer </Text>
+                </TouchableOpacity>
+                
+              </View>
         )
     }
 }
@@ -16,5 +78,8 @@ const styles = StyleSheet.create({
         flex:1,
         justifyContent:'center',
         alignItems:'center',
+    },
+    InputCont: {
+        justifyContent: "space-between"
     }
 })
